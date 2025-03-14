6b3ad7f03a1c409b3c149917f5eae85a504394cd
cecijacogs <cecija@uci.edu>
2025-03-14 05:06:45 +0000

    Code for iterations.

---
+++ src/main.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# runs 1000 times for each (10, 40, and 1000 for 3000 iterations)
+for N in 10 40 4000; do
+    for i in {1..1000}; do
+        python3 src/simulate.py --N $N
+        python3 src/recover.py
+    done
+done
